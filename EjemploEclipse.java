c655e9ff51bd152fe5bc0c250d8d8923e8ee2294
MasterMJGM <162301147+MasterMJGM@users.noreply.github.com>
2024-04-25 19:35:34 +0200

    Añadir multiplicar

--- EjemploEclipse.java
+++ EjemploEclipse.java
@@ -9,6 +9,11 @@ public class enteros {
     public static int resta(int a, int b) {
         return a - b;
     }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
 	
 }
 
